f1e5fe128a24c8a51de3adad88f4cc6536c0f4a9
Mykhailo <dmytrivmykhailo2@gmail.com>
2024-01-15 11:10:21 +0200

    Initial Commit

---
+++ EdutainmentMath/EdutainmentMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentMathApp.swift
+//  EdutainmentMath
+//
+//  Created by Михайло Дмитрів on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
